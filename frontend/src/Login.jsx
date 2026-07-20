cc81d87b4d67dc3d0aad4661f44ad46cb773e0d9
tonyelchediac <tonialchediac@gmail.com>
2026-07-20 13:44:10 +0300

    update forgotten password

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -137,9 +137,6 @@ function Login() {
             <div className="mt-5">
               <div className="flex justify-between">
                 <p className="text-sm font-semibold">Password</p>
-                <a href="" className="text-sm text-blue-600 font-semibold">
-                  Forgot Password?
-                </a>
               </div>
 
               <div className="relative mt-2">
